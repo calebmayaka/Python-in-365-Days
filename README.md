2b158dc3708546cab012383eed538b4ccaa46dff
Caleb Mayaka <mayakaombogo254@gmail.com>
2023-11-13 06:00:52 +0300

    python input function

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Python in 30 Days
- In this repository, i will document learning python in a course of 30 days.
+ In this repository, i will document learning python in the course of 30 days.
---
+++ userinput.py
@@ -0,0 +1,12 @@
+#for taking user input the input() function is used
+
+x = input("enter your name:")    # pass the string you want to be displayed in prompt
+print(x)
+
+user_age = int(input("enter your age: "))           #the datatype is defined when you want an exact data type
+user_name = str(input("enter your full name: "))    
+user_age = str(user_age)            # typecasted this to avoid a concatenation error
+print("your name is " + user_name ," and youre" + user_age ,"years old")
+
+
+
