26a00f9066aa6236e1d426962121e6ce0effd0cf
Caleb Mayaka <mayakaombogo254@gmail.com>
2023-11-14 02:33:38 +0300

    learnt about list functions in python

---
+++ listfunctions.py
@@ -0,0 +1,12 @@
+#there are many list function in python that do different things
+
+        #append
+
+list1 =["caleb", 1,2,3]
+list1.append("mayaka")      #adds the string at the end of the current list
+print(list1)
+
+print(len(list1))
+
+list1.clear()    
+print(list1) 
