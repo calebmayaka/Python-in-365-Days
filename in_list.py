e63bb8d9e8945c2770872c57ad36a8e5fc8ff40f
Caleb Mayaka <mayakaombogo254@gmail.com>
2023-11-14 02:49:11 +0300

    covered in keyword used with if function in lists

---
+++ in_list.py
@@ -0,0 +1,8 @@
+#in is a keyword used to check whether a specific keyword is present in a list
+
+this_list = [1,2,3,4,5,6]
+
+if 2 in this_list:
+    print("Number 2 is present in the list")
+else:
+    print("Not present")
