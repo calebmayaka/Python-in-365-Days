c9d6cf7553d9020041bf8fb50606a052bf9f4ebf
Caleb Mayaka <mayakaombogo254@gmail.com>
2023-11-14 02:48:43 +0300

    covered negative indexing

---
+++ negativeindexing.py
@@ -0,0 +1,9 @@
+#this is a method used to access elements in reverse, from the last to the first
+
+this_list = list(("caleb", "mayaka", "ombogo"))
+
+print(this_list[-1])  # the -1 mens that it will start from the last element
+
+# a range can also be defined
+
+print(this_list[-3:])       #a full colon is used without specifying anything to its right
