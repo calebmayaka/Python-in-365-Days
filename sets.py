5bcc08ca2284e14c5f059d3b2e4893241b198d0e
Caleb Mayaka <mayakaombogo254@gmail.com>
2023-11-14 04:02:14 +0300

    introduction to sets python

---
+++ sets.py
@@ -0,0 +1,10 @@
+# this is another collection data type. uses curly braces. unordered, unindexed and no duplicates.
+#cannot be changed
+
+this_set = {"caleb", "mayaka", "ombogo"}
+print(this_set)
+
+#sets also have a constructor
+newset = set(("mayaka","caleb","Ombogo"))
+
+#also has multiple methods for data manipulation
