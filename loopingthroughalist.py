a8a71841c4cdae5639a40504a095ba11441a35de
Caleb Mayaka <mayakaombogo254@gmail.com>
2023-11-14 03:01:09 +0300

    how to loop through a list using for loop

---
+++ loopingthroughalist.py
@@ -0,0 +1,14 @@
+#list from a constructor
+
+this_newlist = list(("caleb", "Mayaka"))
+
+#using for loop
+
+for mayaka in this_newlist:
+    
+    print("caleb is in the list")
+
+counter = 0
+while counter < 20:
+    print("test counter")
+    counter +=1
