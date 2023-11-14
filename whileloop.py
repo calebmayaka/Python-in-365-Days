649e1bf7cf6e03b8996392ac600af6ac1024b774
Caleb Mayaka <mayakaombogo254@gmail.com>
2023-11-14 13:42:57 +0300

    introduction to while loops

---
+++ whileloop.py
@@ -0,0 +1,13 @@
+#this prints a statement or executes oem stetements if a condition is still true
+
+student_name  = str(input("enter your name: "))
+# student_age = int(input("enter your name: "))
+counter = 1
+while counter < 20:
+    print(student_name)
+    counter += 1
+    
+
+    
+
+
