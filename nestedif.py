72adb6e77ab885969d874248a02f4926bf0edae1
Caleb Mayaka <mayakaombogo254@gmail.com>
2023-11-14 13:13:58 +0300

    looking at nested if

---
+++ nestedif.py
@@ -0,0 +1,11 @@
+#chains if statements
+
+student_name = str(input("enter your name:"))
+if student_name == "caleb":
+    print("name is caleb")
+    if student_name == "mayaka":
+        print("name is mayaka")
+    else:
+        print("not caleb")
+else:
+    print("not mayaka")
