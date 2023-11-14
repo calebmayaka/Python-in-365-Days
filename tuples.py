4948617bcb7f3192ae8ffb337ba2e4110d05bbef
Caleb Mayaka <mayakaombogo254@gmail.com>
2023-11-14 03:15:42 +0300

    introduction to tuples in python

---
+++ tuples.py
@@ -0,0 +1,10 @@
+#tuples are also used to store multiple items
+# allow duplicates same as lists
+#ordered and unchangable and use rounded brackets
+
+new_tuple = ("caleb", "Mayaka", "Ombogo")       #tuples use square brackets
+print(new_tuple)
+#also have a constructor
+
+new_newtuple = tuple(("caleb","mayaka", "Ombogo"))
+print(new_newtuple)
