6d7a98ac0fb00e4a5a3fdd9c705b4f1b677650d9
Caleb Mayaka <mayakaombogo254@gmail.com>
2023-11-14 04:01:59 +0300

    introduction to dictionaries in python

---
+++ dictionaries.py
@@ -0,0 +1,6 @@
+#stores data in key values pairs - they use curly braces - ordered, changable and does not allow duplicates
+
+this_dict = {"color" : "blue", "name" : "caleb", "DOB" : "2002"}
+print(this_dict)
+
+print(this_dict["color"])       #prints the color  -refer using the key name
