726f570c9d4381bc4ed0832f0f017e29c79d7bf7
Caleb Mayaka <mayakaombogo254@gmail.com>
2023-11-14 13:07:22 +0300

    if statement in python

---
+++ ifconditional.py
@@ -0,0 +1,15 @@
+# if is a conditional statement that executes specific conditions are fulfilled
+student_age = 21
+if student_age > 10:
+    print("student is not inm middle school")
+else:
+    print("student is in middle school")
+
+    #take input from the user
+student_age2 = input("enter student age: ")
+if student_age <= 25:
+    print("student has less than 25 years")
+else:
+    print("student is above 25 years")
+
+
