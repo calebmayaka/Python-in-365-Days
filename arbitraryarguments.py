115ec80f604c4c5fbe6d5ce7f0eec3ef3634cad8
Caleb Mayaka <mayakaombogo254@gmail.com>
2023-11-15 16:12:38 +0300

    arbitrary arguements

--- arbitraryarguments.py
+++ arbitraryarguments.py
@@ -4,3 +4,9 @@ def my_function(*kids):
   print("The youngest child is " + kids[2])
 
 my_function("mayaka", "simwamu", "Mogaka")
+
+# keyword arguments - defining specifically
+def this_function(name1, name2, name3):
+  print("name of the children are: " +name1 +name2 +name3)
+
+  this_function(name1 = "caleb", name2 = "mayaka", name3 = "ombogo")
