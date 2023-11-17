e5ebeb4ac8bdd666ea7b761e57db398a4d6c4d27
Caleb Mayaka <mayakaombogo254@gmail.com>
2023-11-17 00:09:22 -0800

    more on random module

--- importrandom.py
+++ importrandom.py
@@ -16,3 +16,20 @@ value3 = random.choice(greetings)
 
 print(f"{value3}, caleb")
 
+#to select multiple random results, the choices is used with a value of k
+
+value4 = random.choices(greetings, k=10)
+print(value4)
+
+#to define the probability of each of the greetings selected, we define weights
+
+value5 = random.choices(greetings,weights=[10,10,2,2], k=10)
+print(value5)
+
+# range keyword - used to create number in a particular range
+
+x = list(range(1,100))          #100 is exclusive
+
+new_list = random.shuffle(x)        #shuffles the list
+print(x)
+
