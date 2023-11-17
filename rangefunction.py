7f02c571efc9e37f35f926e2f6a85275f796bf7f
Caleb Mayaka <mayakaombogo254@gmail.com>
2023-11-17 00:18:57 -0800

    looking at the range function

---
+++ rangefunction.py
@@ -0,0 +1,16 @@
+# this is a function that is used to generate number in a range
+import random           #import random module 
+
+for i in range(1,100):          #loops and prints the number in order
+    print(i)
+
+# here we store the number in a list using a list constructor
+x = list(range(1,100))      #created a list of number between 1 and 100
+
+# print(x)            #prints out the list
+
+# i can randomize the number in the list using the random module
+
+randomized = random.shuffle(x)
+
+print(x)
