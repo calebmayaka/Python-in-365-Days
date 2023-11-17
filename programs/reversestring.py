310d13669f73869af1352f7e4746db5c034bc71e
Caleb Mayaka <mayakaombogo254@gmail.com>
2023-11-18 00:46:18 +0300

    python program that reverses a string

---
+++ programs/reversestring.py
@@ -0,0 +1,9 @@
+
+firstname = str(input("Enter your First Name: "))
+lastname = str(input("Enter your Last Name "))
+
+firstname_reversed = firstname[::-1]
+lastname_reversed = lastname[::-1]
+
+
+print(f"{firstname_reversed} {lastname_reversed}")
