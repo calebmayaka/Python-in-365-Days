188100871975c42d86ec0904b80e9a06f60845a3
Caleb Mayaka <mayakaombogo254@gmail.com>
2023-11-18 02:56:16 +0300

    arithmetic

---
+++ programs/arithmetic.py
@@ -0,0 +1,14 @@
+
+number1 = 10
+number2 = 20
+
+addition = number1 + number2
+subtr = number2 - number1
+product = number1 * number2
+division = number2  / number1
+
+print(f"the addition of the numbers is {addition} and the subtraction is {subtr} and the product is {product} and the division is {division}")
+
+x = number1 == number2
+
+print(x)            
