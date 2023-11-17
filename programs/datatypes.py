c2366bd2031e867295b307598b8078e73f3bac40
Caleb Mayaka <mayakaombogo254@gmail.com>
2023-11-18 02:55:43 +0300

    revising datatypes

---
+++ programs/datatypes.py
@@ -0,0 +1,12 @@
+number1 = 100
+string1 = "caleb"
+float1 = 3.1455
+
+#type is a method for printing out the data type.
+
+x = type(number1)
+print(f"the data type is : {type(number1)} ")
+print(f"the data type is : {type(string1)} ")
+print(f"the data type is : {type(float1)} ")
+
+print(x)
