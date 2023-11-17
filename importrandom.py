c7820995fceb220965fd3ea343bc6e3c5db234f1
Caleb Mayaka <mayakaombogo254@gmail.com>
2023-11-16 23:51:40 -0800

    More on random module

--- importrandom.py
+++ importrandom.py
@@ -1,4 +1,18 @@
-import random
+import random                   #importing the random module which contains the functions necessary for generating and working with random numbers
 
-value = random.random()
+value = random.random()          #calling the random function, it prints a random number between 1 and  by default              
 print(value)
+
+#to get random integers
+
+value2 = random.randint(1, 8)
+print(value2)
+
+# if we want to choose a random greeting from a list of greeitngs
+
+greetings = ["hello", "Morning", "habari","Ssup"]
+
+value3 = random.choice(greetings)
+
+print(f"{value3}, caleb")
+
