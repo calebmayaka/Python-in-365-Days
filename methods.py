34e5dd37cb3e2acb9f9ab5f4a7e18738fe8c7214
Caleb Mayaka <mayakaombogo254@gmail.com>
2023-11-17 21:42:13 +0300

    looking at methods in python

--- methods.py
+++ methods.py
@@ -1,2 +1,24 @@
-#methods are sort of functions associated with a particular object
-"""for example in the random module has a number of functions such as random,shuffle,sample"""
+            #functions that are defined inside a class in python are called methods
+            #methods are sort of functions associated with a particular object
+            #for example in the random module has a number of functions such as random,shuffle,sample
+# methods, by definition, are functions that are associated with classes. They are accessed via instances or the class itself and are designed to operate on the attributes and data within those instances or the class.
+
+# Defining a simple class
+
+
+class Dog:                          #creating a class
+    def __init__(self, name, age):  #initializes class constituents
+        self.name = name            
+        self.age = age
+
+    def bark(self):                 #creating an instance method bark
+        return "Woof!"
+
+# Creating instances (objects) of the Dog class
+dog1 = Dog("Buddy", 3)
+dog2 = Dog("Max", 5)
+
+# Accessing attributes and calling methods of objects for both the variables and methods           
+print(dog1.name)  # Output: Buddy
+print(dog2.age)   # Output: 5
+print(dog1.bark())  # Output: Woof!
