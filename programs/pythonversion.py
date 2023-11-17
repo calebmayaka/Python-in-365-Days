c5b65a0b18993aa7c635285480aab6fe92ecf401
Caleb Mayaka <mayakaombogo254@gmail.com>
2023-11-18 00:46:00 +0300

    program that prints python date and time

---
+++ programs/pythonversion.py
@@ -0,0 +1,7 @@
+# python program to print python version
+import sys          #importing the system module to access system level parameters
+
+print("python version")
+
+version = sys.version
+print(version)
