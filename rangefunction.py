7432eeba09d9a98628eb0a8398ebce6a92b36a13
Caleb Mayaka <mayakaombogo254@gmail.com>
2023-11-17 17:12:24 -0800

    using the range function

--- rangefunction.py
+++ rangefunction.py
@@ -7,10 +7,10 @@ for i in range(1,100):          #loops and prints the number in order
 # here we store the number in a list using a list constructor
 x = list(range(1,100))      #created a list of number between 1 and 100
 
-# print(x)            #prints out the list
+print(x)            #prints out the list
 
 # i can randomize the number in the list using the random module
 
-randomized = random.shuffle(x)
+randomized = random.shuffle(x)          #using the shuffle method
 
 print(x)
