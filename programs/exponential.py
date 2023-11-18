ae3d9c49d32c9e22f9e70d00f43807a5fb963584
Caleb Mayaka <mayakaombogo254@gmail.com>
2023-11-18 21:28:11 +0300

    Create exponential.py

---
+++ programs/exponential.py
@@ -0,0 +1,10 @@
+def exponent(number1):
+    result = number1 ** number1
+    return result
+
+input_1 = int(input("enter the number you want to finr the exponent: "))
+
+x = exponent(input_1)
+
+print(x)
+    
