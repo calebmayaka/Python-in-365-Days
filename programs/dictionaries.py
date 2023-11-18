412daaaaa7514f76bfecf28fd7ce459d60a0a17f
Caleb Mayaka <mayakaombogo254@gmail.com>
2023-11-18 21:28:08 +0300

    working with dictionaries

---
+++ programs/dictionaries.py
@@ -0,0 +1,9 @@
+dict1 = {"name":"caleb", "school": "eksa"}      #dictionaries labels and content are separated by full colon, and this labels are used to refer/ access the contents
+
+
+
+print(dict1["name"])
+
+dict1["name"] = "mayaka"
+
+print(dict1["name"])            #dictionaries are changaeble
