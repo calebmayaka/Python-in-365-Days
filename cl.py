de68fb1b001da0cf1c52df908471e3f71d9e2273
Caleb Mayaka <mayakaombogo254@gmail.com>
2023-12-01 11:20:34 +0300

    Update cl.py

---
+++ cl.py
@@ -0,0 +1,9 @@
+class book:
+    def __init__(self,name,author):
+        self.name = name
+        self.author = author
+        
+    def Details (self):
+        print(f"the book name is {self.name} and the author is {self.author}")
+        
+        
