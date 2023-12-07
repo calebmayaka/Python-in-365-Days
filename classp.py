0047dad794fae2c3e8da1df6965500b8ee15abef
Caleb Mayaka <mayakaombogo254@gmail.com>
2023-12-07 04:47:57 +0300

    Update classp.py

---
+++ classp.py
@@ -0,0 +1,16 @@
+class book:
+    
+    book_counter = 0
+    def _init__(self,name,author):
+        self.name = name
+        self.author = author
+    
+    book_counter += 1
+    
+    def nameandauthor(self):
+        print(f"the name of the book is {self.name} and the author is {self.author}")
+    
+book1 = 
+        
+
+        
