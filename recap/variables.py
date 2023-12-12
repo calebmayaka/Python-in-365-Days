7fda633a2cf72b25268ca41a05297ded13650de6
Caleb Mayaka <mayakaombogo254@gmail.com>
2023-12-12 05:11:20 +0300

    Update variables.py

--- recap/variables.py
+++ recap/variables.py
@@ -12,3 +12,13 @@ print(type(float_number))
 x  = 250
 
 print(x)
+
+#multiple assingment - asings multiple variables in one
+
+a,b,c = 10,20,30
+
+print(a,b,c)
+
+# constants - in python thereare no specific method of defining constants but theey are indicated by all caps variable names
+
+PI  = 3.142
