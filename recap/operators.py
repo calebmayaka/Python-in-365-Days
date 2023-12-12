7cc8d8c4e73ac9095beb933fc5ac9d83357cbcdd
Caleb Mayaka <mayakaombogo254@gmail.com>
2023-12-12 05:19:16 +0300

    Create operators.py

---
+++ recap/operators.py
@@ -0,0 +1,23 @@
+#there are multiple types of operators 
+
+# arithmetic operators
+x = 10
+y = 20
+
+addition = x + y 
+subtraction = x - y
+division  = x / y
+multiplication = x * y
+
+print(addition,subtraction,division,multiplication)
+
+# logical operators
+
+"""== -  this is for checking whether they are equal
+        =! - returns true if they are not equal """
+
+while x == y:
+    print("they are equal")
+else:
+    print("they arent equal")
+
