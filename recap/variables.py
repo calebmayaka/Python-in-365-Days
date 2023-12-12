c2c630477733c6a09c3f3530d7b85a734e00115f
Caleb Mayaka <mayakaombogo254@gmail.com>
2023-12-12 05:07:34 +0300

    variables recap

---
+++ recap/variables.py
@@ -0,0 +1,9 @@
+x = 5  # this is a integer
+name_student = "mayakaombogo" # this is a string
+float_number = 2.6
+
+#these are the most used data types in python
+
+print(type(x))
+print(type(name_student))
+print(type(float_number))
