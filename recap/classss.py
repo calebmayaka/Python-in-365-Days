af0003f6c4df076dd1ca2df097a87b2939ad1cde
Caleb Mayaka <mayakaombogo254@gmail.com>
2024-01-31 20:27:34 +0300

    Update classss.py

---
+++ recap/classss.py
@@ -0,0 +1,19 @@
+class book:
+    number_book = 0
+    def __init__(self,bookname, bookauthor):
+        self.bookname = bookname
+        self.bookauthor  = bookauthor
+        self.number_book += 1
+        
+    def printbook_name(self):
+        print(f"The bookname is {self.bookname} and the name of the author is {self.bookauthor}")
+        
+
+
+book1 = ("mayaka", "introduction to python")
+
+print(self.book1)
+        
+        
+        
+    
