45b101dd389e3dfc2c156daf0746c784bf0b43cb
Caleb Mayaka <mayakaombogo254@gmail.com>
2024-05-08 10:25:04 +0300

    Update numbers.py

--- recap_py/numbers.py
+++ recap_py/numbers.py
@@ -2,3 +2,28 @@
 # integers
 # floats
 # complex
+
+# integers - they incude both negative and positive numbers of all sizes
+
+a = 512
+
+# float - contains decimals
+b = 3.142
+
+# complex
+c = 1j
+
+print(type(c))
+
+# type conversion
+
+x = 250
+y = 3.1742
+
+g =int(y)
+
+print(g)
+
+import random
+
+print(random.randrange(1,156444654654))
