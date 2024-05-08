2a2db1de6fb6e32f0de5cddcbc53bceee27a5b23
Caleb Mayaka <mayakaombogo254@gmail.com>
2024-05-08 08:58:52 +0300

    Update comments.py

--- recap_py/comments.py
+++ recap_py/comments.py
@@ -1,5 +1,12 @@
-# a = 250   # this is a single line comment
+a = 250   # this is a single line comment
 if a > 100:
     print("the number is greater than 100")
 else:
     print("the number is less 100")
+
+""" if a > 100:
+    print("the number is greater than 100")
+else:
+    print("the number is less 100")
+"""
+# above is a mulitline comment also know as docstring
