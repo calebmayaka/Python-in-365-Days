fca732d2789845023ec01a9ab6bfd86a9c127262
Caleb Mayaka <mayakaombogo254@gmail.com>
2024-05-08 09:48:29 +0300

    Update globalvariables.py

--- recap_py/globalvariables.py
+++ recap_py/globalvariables.py
@@ -1,7 +1,17 @@
 # these are variables that are defined outsid ea function and they they can be accessed from anywhere
+
 a = "incredible"
 
 def function1():
         print("python is " + a)
 
 function1()
+
+# # the global keyword can be used to define a global variable inside a function
+
+# def function2():
+#         global b
+#         b = 1000
+
+
+# print(b)
