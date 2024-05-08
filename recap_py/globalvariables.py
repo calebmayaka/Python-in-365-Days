b2904a1cfeadbf7d5bf508a683fb61f4ec4601b6
Caleb Mayaka <mayakaombogo254@gmail.com>
2024-05-08 09:42:39 +0300

    Create globalvariables.py

---
+++ recap_py/globalvariables.py
@@ -0,0 +1,7 @@
+# these are variables that are defined outsid ea function and they they can be accessed from anywhere
+a = "incredible"
+
+def function1():
+        print("python is " + a)
+
+function1()
