d1a693d58274eace835f8b79b3e3fb59e6534561
Caleb Mayaka <mayakaombogo254@gmail.com>
2024-05-08 11:48:45 +0300

    Create random2.py

---
+++ recap_py/random2.py
@@ -0,0 +1,15 @@
+import random as rd
+
+x = rd.random()
+
+y = rd.randrange(1,250)
+
+print(x, y)
+
+# selecting a random from a list using choice
+
+list1 = ["caleb", " mayaka", "ombogo","callycalex"]
+
+print(type(list1))
+
+print(rd.choice(list1))
