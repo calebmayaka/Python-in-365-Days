30559f947ded6785d2cd6f42c98e18ebff1d4f53
Caleb Mayaka <mayakaombogo254@gmail.com>
2024-05-08 10:52:21 +0300

    Create strings.py

---
+++ recap_py/strings.py
@@ -0,0 +1,28 @@
+# strings are sorrounded by either quotation marks or double quotation marks
+# defining a string
+a = "caleb"
+
+b = """my name is mayaka ombogo and i am a 
+software engineer who writes python, django, javascript and rust"""
+
+print(b)
+
+
+# looping through a string
+x = "mayakaombogo"
+
+for m in x:
+    print(" m is in x")
+
+# length of a string
+
+print(len(x))
+
+
+# check string using the in keyword
+
+txt = "The best things in life are free!"
+if "free" in txt:
+    print("Yes, 'free' is present.")
+else:
+    print("free is not present")
