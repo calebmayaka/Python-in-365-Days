e623ce8edbde14b89bb19953512ca32e908b74ff
Caleb Mayaka <mayakaombogo254@gmail.com>
2024-05-08 09:21:48 +0300

    Create formatted strings.py

---
+++ recap_py/formatted strings.py
@@ -0,0 +1,5 @@
+# formatted strings are used to make it easy to call variables when printing
+str1 = "mayaka"
+str2 = 'caleb'
+# f is placed before the opening quotation marks and then the variable names are enclosed between curly braces
+print( f"my name is {str1} {str2}")
