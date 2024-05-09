093cc7cf35426488b731e8ac82ac0ecc90690a87
Caleb Mayaka <mayakaombogo254@gmail.com>
2024-05-09 09:01:51 +0300

    Create rp.py

---
+++ recap_py/rp.py
@@ -0,0 +1,37 @@
+a = 250 # this is an integer
+b = "caleb" # string - can be double or single quotation marks
+c = 3.142 # float
+
+"""triple quotation marks denote multi line comments also
+ called docstrings"""
+
+#typecasting
+x = int(c)
+print(x)
+
+y = str(15)
+print(type(y))
+
+# python check
+
+#python --version
+
+# formatted strings
+
+print(f"the value of a is {a} and b is {b} and c is {c}")
+
+# defining multiple variables
+
+g,h,j = "mayaka", "caleb", "ombogo"
+
+print(g)
+print(h)
+print(j)
+
+# assigning multiple variables to the same value
+
+u=i=o = "callycalex"
+
+print(u,i,o)
+
+#
