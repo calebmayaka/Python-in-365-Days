611cdf6e82b869da5d7f2f88bd7c8379ae391254
Caleb Mayaka <mayakaombogo254@gmail.com>
2024-05-09 09:55:23 +0300

    Create concatenation.py

---
+++ recap_py/concatenation.py
@@ -0,0 +1,14 @@
+# a = "caleb"
+# b = "mayaka"
+# x = a + " " + b
+# print(x)
+
+
+first_name = "caleb"
+last_name  = "mayaka"
+
+full_name = first_name + " " + last_name
+
+print(full_name)
+
+# escaping characters
