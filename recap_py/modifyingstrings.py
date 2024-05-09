59a03ff4a30443b1db63df733fba1f2660c0d416
Caleb Mayaka <mayakaombogo254@gmail.com>
2024-05-09 09:55:26 +0300

    Create modifyingstrings.py

---
+++ recap_py/modifyingstrings.py
@@ -0,0 +1,19 @@
+str_1 = "caleb  "
+
+str_1 = str_1.upper()
+
+print(str_1.lower())
+
+# removing whitespaces
+
+str_1.strip()
+
+print(str_1)
+
+# replace letter
+
+print(str_1.replace("C", "Z"))
+
+# split
+
+print(str_1.split("l"))
