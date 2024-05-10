17b85969015a475a21e1203aca7df5dd241f6c3a
Caleb Mayaka <mayakaombogo254@gmail.com>
2024-05-10 10:07:35 +0300

    Create boolean.py

---
+++ recap_py/boolean.py
@@ -0,0 +1,27 @@
+# boolean are either True of False - Both start with upper case letter
+
+x = True
+y = False
+
+a = 100
+
+if a < 100:
+    print(f" {a} is less than 100")
+else:
+    print(f"{a} is greater than 100")
+
+if a < 100:
+    print(x)
+else:
+    print(y)
+    
+print(100>150)
+print(1000<2500)
+
+# bool function can be used to evaluate and return bool value, all variables with content return true except empty
+
+print(bool(a))
+
+w = 0
+
+print(bool(w))
