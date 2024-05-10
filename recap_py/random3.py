75c2df43c1b392d7719357dc27d48697432c9e16
Caleb Mayaka <mayakaombogo254@gmail.com>
2024-05-10 10:07:37 +0300

    Update random3.py

--- recap_py/random3.py
+++ recap_py/random3.py
@@ -1,8 +1,6 @@
 import random as rd
-import tkinter as tk
+# import tkinter as tk
 
-print(rd.randrange(1, 545465))
+value = rd.random()
 
-value =  rd.random()
-value =  int(value ** value + 10)
 print(value)
