04dab21abf38dc17e88758b1118b9f73c0ad4ab6
Caleb Mayaka <mayakaombogo254@gmail.com>
2024-05-12 10:23:35 +0300

    Create loops.py

---
+++ recap_py/loops.py
@@ -0,0 +1,15 @@
+# counter = 1
+# your_name = str(input("enter your name: "))
+# while counter <=100:
+#     print(your_name)
+#     counter = counter + 1
+
+list1 = ["caleb", "mayaka", "ombogo"]
+
+for caleb in list1:
+    print("Caleb is present")
+else: 
+    print("caleb is absent")
+     
+
+    
