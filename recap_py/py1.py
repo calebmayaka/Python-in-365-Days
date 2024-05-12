d5f080889a418e2b651aa0908696bfc7a7379f62
Caleb Mayaka <mayakaombogo254@gmail.com>
2024-05-12 09:53:42 +0300

    Update py1.py

--- recap_py/py1.py
+++ recap_py/py1.py
@@ -1,9 +1,25 @@
 a = 100
 b = "caleb"
+x = "OMBOGO"
 c = True
 
+# function that converts the string in variable b to uppercase
 def toUpper():
-    A = b.upper()
+    B = b.upper()
+    print(B)
 
 toUpper()
+
+def toLower():
+    X = x.lower()
+    print(X)
+
+toLower()
+
+def printUserName():
+    print(f" Good Morning {toUpper()} {toLower()}")
+
+printUserName()
+
+
 
