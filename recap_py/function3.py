70c6da7ac4571d9b557cd5d9e791c110d9d7a213
Caleb Mayaka <mayakaombogo254@gmail.com>
2024-05-13 17:23:51 +0300

    update on functions

---
+++ recap_py/function3.py
@@ -0,0 +1,16 @@
+# function 1
+
+def func():
+    # take user input
+   x = int(input(" enter your age: "))
+   y = 100
+   while x > y:
+     print("x is greater than y")
+     x =  x + 10
+     break
+
+
+func()
+
+
+   
