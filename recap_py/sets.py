536c25a10335565ee7eb758e67427f39884f8bd8
Caleb Mayaka <mayakaombogo254@gmail.com>
2024-05-14 12:37:28 +0300

    Create sets.py

---
+++ recap_py/sets.py
@@ -0,0 +1,10 @@
+# another collection data type
+# values cannot be changed
+# does not accept duplicates
+set_1  = {"caleb", " mayaka", 1 , "ombogo"}
+
+print(set_1)
+
+# set constructor
+
+this_set = set(("caleb","mayaka","ombogo"))
