37f9b1a3fb4aeb11c26abbe93b2113dd145ea623
Caleb Mayaka <mayakaombogo254@gmail.com>
2024-05-14 12:37:25 +0300

    Create lists.py

---
+++ recap_py/lists.py
@@ -0,0 +1,18 @@
+# Lists are a collection data type in python
+
+list_1  = ["caleb", 80, "ombogo"]
+print(list_1)
+
+print(list_1[0], list_1[1]) # printing a specific index
+
+# changing a value in a list in a specific index
+
+list_1[0] = "ombogomayaka"
+
+print(list_1[0]) 
+
+
+# list functions
+
+# pop removes a specified index
+list_1.pop(0)
