9d24c93c62e239ce0dc38f06db98d3f76022c395
Caleb Mayaka <mayakaombogo254@gmail.com>
2024-05-14 13:05:41 +0300

    Create function4.py

---
+++ recap_py/function4.py
@@ -0,0 +1,26 @@
+# functions are blocks of code executed as one when called
+
+# def keyword is used
+
+#   arguements can be passed to the function
+
+def print_name_age(name_caleb, age):
+    print(f"my name is {name_caleb} and i am {age} years old")
+
+print_name_age("caleb", 21)
+
+print_name_age("ombogo", 20)
+
+print_name_age("ruth", 24)
+
+
+
+def greetings(name_, time_of_the_day):
+    print(f"Good {time_of_the_day} {name_} ")
+
+
+greetings("caleb", "Morning")
+greetings("mayaka", "afternoon")
+greetings("ombogo", "evening")
+
+    
