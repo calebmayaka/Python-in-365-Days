682c2b66e825e3bda12fe3d265f5b3937c18df09
Caleb Mayaka <mayakaombogo254@gmail.com>
2024-05-14 14:40:11 +0300

    Update input.py

---
+++ recap_py/input.py
@@ -0,0 +1,9 @@
+import random as rd
+caleb_age = 21
+caleb_name = "caleb"
+
+def greeting():
+    greetings_list = ["hello", "howdy", "habari"]
+    
+
+print(f"my name is {caleb_name}, my age is {caleb_age}")
