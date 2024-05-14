0c3390f376d1cdf709454eac626730ba50cdd37c
Caleb Mayaka <mayakaombogo254@gmail.com>
2024-05-14 14:40:08 +0300

    Create function5.py

---
+++ recap_py/function5.py
@@ -0,0 +1,11 @@
+
+
+def function1(x,y):
+    print(f"my name is {y} and i am {x} years old")
+
+age = int(input("enter your age: "))
+name_ = str(input("enter your name: "))
+
+
+
+function1(age, name_)
