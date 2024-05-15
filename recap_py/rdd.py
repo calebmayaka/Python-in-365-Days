53997736788e9c126c8908b7e7b2d327551e4154
Caleb Mayaka <mayakaombogo254@gmail.com>
2024-05-15 10:08:47 +0300

    Create rdd.py

---
+++ recap_py/rdd.py
@@ -0,0 +1,21 @@
+# # import random as rd
+# # u  = rd.random()
+# # print(u)
+
+# # # list constructor
+
+# list_1  = list(("caleb", "mayaka", 1))
+
+# print(list_1[0])
+
+# list_1.pop[1]
+
+# function to handle talking user inout
+
+def userinput(name, adress):
+    s = str(input("name: "))
+    y = int(input("address: "))
+    print(f"{s} {y}")
+
+userinput("caleb", 17040501)
+
