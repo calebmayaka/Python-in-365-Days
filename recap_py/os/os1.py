8caa27942c28a6283378f2fe9ff23587971795c7
Caleb Mayaka <mayakaombogo254@gmail.com>
2024-05-15 14:51:22 +0300

    Update os1.py

--- recap_py/os/os1.py
+++ recap_py/os/os1.py
@@ -5,10 +5,23 @@ print(os.getcwd())
 
 # os.chdir("/users/caleb")  # changes the working directory
 
-print(os.listdir())
+# print(os.listdir())
 
 # os.mkdir("osdir__")
 # os.makedirs("osdir1/dir1")
 
-os.rmdir("osdir")
-# os.removedirs("osdir1/dir1")
+# os.rmdir("osdir") # will not remove intermediate directories
+
+# os.removedirs("osdir1/dir1")  # will remove all in the directory
+
+os.walk("") # lists 3 value tuple of path, dir name and filenames
+
+# rename
+
+# os.rename("test.txt" , "new.txt")
+
+print(os.stat("datatypes.py"))
+
+# environment variables
+
+print(os.environ.get("HOME"))
