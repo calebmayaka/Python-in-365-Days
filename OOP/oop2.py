abccc4a4821a5537bb85ec02d412e51ed86d964f
Caleb Mayaka <mayakaombogo254@gmail.com>
2024-05-16 17:18:18 +0300

    Create oop2.py

---
+++ OOP/oop2.py
@@ -0,0 +1,24 @@
+# @classmethod
+
+import random as rd
+
+
+x = rd.random()
+
+print(x)
+
+y = rd.randrange(1,50000)
+
+print(y)
+
+def caleb(name,age):
+    print(f"my name is {name} and i am {age} old")
+
+
+
+caleb("mayakaombogo", 21)
+
+class cally:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age 
