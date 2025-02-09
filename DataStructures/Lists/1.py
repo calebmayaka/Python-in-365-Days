e15f1fae8f42b4bac3e0c91802c350552e0491c8
Caleb Mayaka <mayakaombogo254@gmail.com>
2025-02-09 23:54:11 +0300

    update

---
+++ DataStructures/Lists/1.py
@@ -0,0 +1,19 @@
+# empty list
+list_1 = []
+
+list_2 = ['caleb', 'mayaka','ombogo',0]
+
+list_1.append('caleb')
+
+list_2.remove(0)
+list_3 = [1,2,3,4,5,6,7,8,9,10]
+
+del list_3[0]
+
+print(list_3)
+
+list_2.pop(1)
+
+print(list_1[0])
+
+print(list_2)
---
+++ DataStructures/Lists/2.py
@@ -0,0 +1,11 @@
+list_1 = [0,1,2,3,4,5,6,7,8,9]
+
+# append - add to the end of the list
+list_1.append(10)
+print(list_1)
+
+# delete an entire list
+list_2 = [1]
+del list_2
+print(list_2)
+
